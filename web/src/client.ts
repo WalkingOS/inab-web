7c9fadef74a2e35d8ef41179d097d31df425cc3b
Osman <osman.minaz@hof-university.de>
2021-10-10 00:46:52 +0200

    init storybook + sanity

---
+++ web/src/client.ts
@@ -0,0 +1,9 @@
+//npm install @sanity/client
+// React to Sanity 
+
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+    projectId: "neqvyx3y",
+    dataset: "production"
+})
